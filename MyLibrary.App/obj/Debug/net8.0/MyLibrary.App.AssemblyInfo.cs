2a006ee30a6022a33bfb208d73010bb4d78c95b8
FelipeS3 <Felipexy50@gmail.com>
2025-01-15 23:13:46 -0300

    PrimeiroCommit

--- MyLibrary.App/obj/Debug/net8.0/MyLibrary.App.AssemblyInfo.cs
+++ MyLibrary.App/obj/Debug/net8.0/MyLibrary.App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyLibrary.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7443b8739a496ac2f272c789fef9af45ab9ac720")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyLibrary.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyLibrary.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
